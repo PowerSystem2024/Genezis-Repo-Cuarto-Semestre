67079ca81e2506b7a49a869c9134dc2d22e049cb
Gabriel <gabrielgarino10@gmail.com>
2025-08-30 22:35:53 -0300

    Subiendo la Clase 3 de Node.js al repo grupal

---
+++ Node/Clase 3/app2.js
@@ -0,0 +1,8 @@
+const saludar = (nombre) => { //Función de flecha
+    return `Saludos de ${nombre}`; //Usamos back tic`s
+}//esto es igual a: 'saludos' + nombre;
+console.log(saludar('Gabriel'));
+
+
+
+
