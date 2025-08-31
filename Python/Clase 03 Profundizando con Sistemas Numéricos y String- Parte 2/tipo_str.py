a1117cad5d4dfd98933b49352e0e9f220bfc5bfb
Gabriel <gabrielgarino10@gmail.com>
2025-08-30 22:43:04 -0300

    Subiendo la Clase 03 de Python al repo grupal

---
+++ Python/Clase 03 Profundizando con Sistemas Numéricos y String- Parte 2/tipo_str.py
@@ -0,0 +1,28 @@
+import math
+# Profundizando en el tipo string
+# Concatenación automática en Python
+
+variable = ' Adios'
+mensaje = 'Hola '' Alumnos' + variable
+mensaje += ', Terminamos'
+# print(mensaje)
+
+# Usamos la clase help pra ayuda o para documentación (built-in)
+help(math.isnan)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
