1319b91d1b7225036bd2c23eef2119c646dd88e8
Gabriel <gabrielgarino10@gmail.com>
2025-09-11 13:51:57 -0300

    Subiendo al repo grupal la Clase 03 Aplicación de Sistema Estudiantes con Spring Boot - Parte 2 en Java

---
+++ Java/Clase 03 Aplicación de Sistema Estudiantes con Spring Boot - Parte 2/java/utn/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,6 @@
+
+package utn.estudiantes.modelo;
+
+public class Estudiante {
+    
+}
---
+++ Java/Clase 03 Aplicación de Sistema Estudiantes con Spring Boot - Parte 2/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utn.estudiantes.repositorio;
+
+/**
+ *
+ * @author garin
+ */
+public class EstudianteRepositorio {
+    
+}
